0a91873c264899de9d6f7d293d52d4540c34113c
Opilane TTHK <opilane@tthk.ee>
2024-11-05 14:16:46 +0200

    Add XML file and SQL file [Delivers #188525790]

---
+++ XML/veeNaidud.sql
@@ -0,0 +1,118 @@
+create table veeNaidud (
+	id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	email VARCHAR(50),
+	korterinumber VARCHAR(50),
+	kuupaev DATE,
+	külmVesi INT,
+	soeVesi INT,
+	makstud BIT
+);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('tpilsworth0@businessweek.com', '59721', '2024-05-13', 16105, 74745, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('madrienne1@blogs.com', '92301', '2024-07-28', 74973, 60852, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('amattioni2@earthlink.net', '36399', '2023-12-24', 63509, 35452, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('jgabbatt3@guardian.co.uk', '5', '2024-01-15', 86295, 26350, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('slown4@bing.com', '69413', '2024-03-19', 69945, 30674, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('aobington5@usgs.gov', '5625', '2024-08-02', 83790, 16252, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('ccream6@drupal.org', '6303', '2024-01-08', 51834, 22555, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('tbockman7@statcounter.com', '19233', '2024-10-01', 54607, 85328, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('briding8@ustream.tv', '3', '2023-11-30', 64430, 5968, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('dskirving9@fema.gov', '28', '2024-02-03', 43635, 84983, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('fstollmeiera@csmonitor.com', '3', '2024-04-29', 17050, 20745, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('kalenichevb@umich.edu', '29534', '2024-01-02', 82605, 11015, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('tmarwoodc@flavors.me', '49365', '2024-10-11', 26701, 62047, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('xwainscotd@tuttocitta.it', '8', '2024-07-26', 54735, 70604, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('ktallowine@google.cn', '114', '2024-05-25', 55261, 76662, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('nantonssonf@ovh.net', '93260', '2024-03-08', 34592, 82274, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('agarting@arstechnica.com', '07147', '2024-02-14', 28306, 24811, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('hgoadh@sun.com', '680', '2024-01-31', 32014, 21540, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('ltinglei@delicious.com', '97034', '2024-09-17', 38202, 69346, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('hmanniej@reference.com', '51', '2024-11-04', 8969, 96310, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('cnawtonk@skyrock.com', '4', '2024-06-03', 54207, 64564, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('bmarchingtonl@cnn.com', '028', '2024-02-20', 79112, 77088, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('rcrightonm@people.com.cn', '59733', '2024-03-13', 28118, 48434, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('rmarkwickn@amazonaws.com', '7040', '2024-04-13', 94901, 9372, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('jsperskio@hp.com', '33', '2024-04-13', 59825, 11642, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('bharvattp@wired.com', '57', '2024-02-05', 19849, 12986, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('kcavaneyq@ibm.com', '419', '2024-01-25', 28083, 29491, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('mburchfieldr@answers.com', '98', '2024-03-12', 3204, 83193, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('mblydes@msu.edu', '455', '2024-08-08', 66319, 71392, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('bknappettt@netlog.com', '18', '2024-06-06', 27116, 26434, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('gfontellu@umich.edu', '9', '2024-06-05', 25198, 6849, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('afanninv@storify.com', '595', '2024-03-10', 67510, 57588, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('jdumphreysw@netscape.com', '45', '2024-10-29', 89439, 28919, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('mjulianx@wordpress.org', '30', '2024-08-26', 12129, 20400, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('rmalpassy@homestead.com', '54', '2023-11-10', 78300, 64670, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('mmatiasekz@simplemachines.org', '010', '2024-08-04', 15706, 82308, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('adavioud10@mapquest.com', '96', '2024-01-13', 47747, 68962, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('tpafford11@addtoany.com', '80482', '2024-01-11', 24598, 978, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('gmault12@hc360.com', '5', '2024-03-20', 31383, 75233, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('adodsley13@msu.edu', '26370', '2024-03-04', 55274, 64423, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('clamshead14@loc.gov', '03521', '2023-12-21', 9605, 23430, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('npaula15@google.pl', '6', '2023-11-11', 82537, 11539, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('esavine16@about.com', '461', '2024-02-19', 12668, 1863, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('opincked17@netlog.com', '938', '2024-08-25', 40871, 905, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('sheadon18@mlb.com', '73597', '2024-07-09', 37363, 81129, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('bnucciotti19@printfriendly.com', '4', '2024-02-23', 65408, 29585, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('troderick1a@hostgator.com', '93709', '2024-10-15', 7848, 28177, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('gglossop1b@walmart.com', '4255', '2024-08-06', 86224, 19863, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('nsheara1c@gmpg.org', '51', '2024-01-29', 8019, 78285, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('nmawer1d@diigo.com', '8', '2024-05-18', 24154, 12495, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('reddington1e@webmd.com', '75', '2024-01-29', 44914, 32388, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('waugust1f@walmart.com', '7869', '2024-04-23', 4398, 60492, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('ewhal1g@chronoengine.com', '87602', '2024-01-07', 82418, 35665, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('wpenke1h@drupal.org', '62709', '2024-04-15', 49350, 5661, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('adawtry1i@yelp.com', '872', '2024-03-18', 70093, 13070, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('tshipston1j@friendfeed.com', '35279', '2024-04-24', 30347, 8278, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('brichemont1k@china.com.cn', '2392', '2024-09-06', 33995, 46777, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('kascough1l@ustream.tv', '41992', '2024-06-01', 11246, 86091, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('dbagwell1m@miitbeian.gov.cn', '2173', '2024-08-29', 34384, 33097, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('dklimontovich1n@census.gov', '213', '2024-01-03', 96413, 64113, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('hluffman1o@privacy.gov.au', '28240', '2024-03-28', 87369, 76939, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('eeastup1p@hubpages.com', '693', '2024-08-01', 7018, 40316, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('jgetcliff1q@google.fr', '84', '2024-09-08', 74258, 22586, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('cstreat1r@reverbnation.com', '21', '2024-01-22', 84878, 845, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('jcristofano1s@economist.com', '53906', '2024-05-29', 12670, 94181, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('bdradey1t@over-blog.com', '23579', '2024-03-28', 24340, 63050, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('btolemache1u@canalblog.com', '54', '2024-07-14', 37255, 15357, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('lstruss1v@joomla.org', '81834', '2024-09-17', 73882, 73021, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('bburchell1w@wikimedia.org', '939', '2024-03-22', 10370, 56470, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('hsibthorp1x@shop-pro.jp', '1', '2024-09-20', 45917, 51395, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('scaile1y@wsj.com', '9256', '2024-07-27', 98322, 26477, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('lclemow1z@zdnet.com', '2', '2024-09-26', 77884, 12007, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('rcammomile20@wired.com', '49', '2024-08-19', 55425, 89397, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('hverbruggen21@theglobeandmail.com', '21', '2024-07-01', 75276, 94994, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('crandlesome22@google.com.br', '40', '2024-08-08', 26176, 88496, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('wcheal23@a8.net', '3', '2024-06-24', 3587, 79874, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('lbuckeridge24@wisc.edu', '7', '2024-08-15', 49493, 36292, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('mbuston25@deviantart.com', '171', '2024-01-07', 34894, 73315, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('lpettengell26@bing.com', '7', '2024-05-27', 36378, 56510, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('tcecchetelli27@exblog.jp', '30', '2024-03-10', 57530, 51326, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('crizzotto28@bbc.co.uk', '04020', '2024-08-30', 95026, 56667, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('mscholz29@posterous.com', '30', '2024-10-25', 88980, 67020, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('jkurth2a@uol.com.br', '39', '2024-01-10', 32669, 97836, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('jfriday2b@si.edu', '05600', '2024-02-26', 25930, 5362, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('pthominga2c@diigo.com', '4232', '2023-12-29', 43715, 2072, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('jgostridge2d@feedburner.com', '8216', '2024-04-17', 98994, 32785, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('crawe2e@smugmug.com', '36', '2024-08-15', 86622, 99940, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('patto2f@xrea.com', '23533', '2024-01-05', 12364, 12311, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('mraselles2g@arstechnica.com', '2340', '2024-08-14', 67426, 98449, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('pcosten2h@marketwatch.com', '0662', '2023-11-20', 3744, 50048, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('strimmell2i@indiegogo.com', '429', '2023-11-15', 13496, 59785, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('akoschek2j@cnet.com', '94080', '2023-12-29', 55319, 98911, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('sbraksper2k@sciencedirect.com', '89', '2024-02-15', 54116, 53664, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('qbenoy2l@usnews.com', '8', '2024-04-21', 88926, 8036, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('abrose2m@huffingtonpost.com', '0', '2024-05-12', 95098, 92605, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('ocloy2n@homestead.com', '3', '2024-09-16', 74248, 7122, 0);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('jalsop2o@webs.com', '18888', '2024-04-25', 70128, 55451, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('wgaze2p@opensource.org', '1', '2024-08-29', 47609, 14594, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('dmahody2q@blogs.com', '68', '2024-08-16', 1846, 86824, 1);
+insert into veeNaidud (email, korterinumber, kuupaev, külmVesi, soeVesi, makstud) values ('amccane2r@thetimes.co.uk', '135', '2024-07-03', 6450, 46649, 1);
+
+select 
+email,
+korterinumber,
+kuupaev,
+kulmVesi as 'vesi/kulmVesi',
+soeVesi as 'vesi/soeVesi',
+makstud
+from veeNaidud for xml path('veeNaide'), root('veeNaidud')
